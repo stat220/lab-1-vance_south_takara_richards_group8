acbf63f374ca8b4c8827a2843ab934cab780c98b
waatermelon <hugh.takara@gmail.com>
2024-09-13 11:43:28 -0600

    py - template boilerplate

---
+++ part3-4.py
@@ -0,0 +1,26 @@
+#imports
+import pandas as pd
+
+
+"""
+3. Impact of Company Size on Salary:
+
+    Explore how company size affects data scientist salaries. Generate plots or tables to
+    visualize this relationship.
+"""
+
+
+
+
+
+"""
+4. Job Title Analysis:
+
+    Job titles can vary significantly. For this project, select a keyword or phrase to distinguish between different job titles. For instance, you can differentiate between job
+    titles containing the word ”Analyst” and those that do not. To achieve this, you may
+    find the str.contains function in Pandas helpful. Refer to this resource for examples:
+    https://www.geeksforgeeks.org/python-pandas-series-str-contains. Explore
+    various job titles to decide on the keyword or phrase you want to focus on.
+"""
+
+
