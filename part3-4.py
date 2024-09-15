19180d94b62efbc6a47fdba7380d5f10bb494150
c4zombieslayer <40480983+SuperiorHugh@users.noreply.github.com>
2024-09-14 19:46:56 -0600

    py - updated part3-4, finished

--- part3-4.py
+++ part3-4.py
@@ -1,5 +1,11 @@
 #imports
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
+#data
+data = pd.read_csv('ds_salary.csv')
 
 
 """
@@ -10,7 +16,60 @@ import pandas as pd
 """
 
 
+x = data['salary_in_usd'].values.reshape(-1, 1)
+y = data['company_size'].values
 
+plt.scatter(x, y, label="Scatterplot", )
+plt.title("Company Size on Salary")
+plt.ylabel("Company Size")
+plt.xlabel("Salary (USD)")
+plt.show()
+
+#seems like larger company has higher max pay
+
+grouped = data.groupby('company_size')['salary_in_usd'].apply(list)
+
+small_arr = pd.Series(grouped.get('S', []))
+medium_arr = pd.Series(grouped.get('M', []))
+large_arr = pd.Series(grouped.get('L', []))
+
+plt.hist(small_arr, bins=20)
+plt.title("Salary Distribution for Small Companies")
+plt.xlabel("Salary (USD)")
+plt.xticks([0, 100000, 200000, 300000, 400000, 500000, 600000, 700000])
+plt.ylabel("Frequency")
+plt.show()
+
+print("\nsmall company quartiles")
+for i in range(5):
+  print(f"{i/4} quantile: {small_arr.quantile(i/4)}")
+
+plt.hist(medium_arr, bins=20)
+plt.title("Salary Distribution for Medium Companies")
+plt.xlabel("Salary (USD)")
+plt.xticks([0, 100000, 200000, 300000, 400000, 500000, 600000, 700000])
+plt.ylabel("Frequency")
+plt.show()
+
+print("\nmedium company quartiles")
+for i in range(5):
+  print(f"{i/4} quantile: {medium_arr.quantile(i/4)}")
+
+plt.hist(large_arr, bins=20)
+plt.title("Salary Distribution for Large Companies")
+plt.xlabel("Salary (USD)")
+plt.xticks([0, 100000, 200000, 300000, 400000, 500000, 600000, 700000])
+plt.ylabel("Frequency")
+plt.show()
+
+print("\nlarge company quartiles")
+for i in range(5):
+  print(f"{i/4} quantile: {large_arr.quantile(i/4)}")
+
+
+# The small company pays the least
+# The medium company pays good
+# The large company pays a little less than medium
 
 
 """
@@ -23,4 +82,16 @@ import pandas as pd
     various job titles to decide on the keyword or phrase you want to focus on.
 """
 
+keyword = "Engineer"
 
+contains_keyword = data['job_title'].str.contains(keyword)
+with_keyword = data[contains_keyword]
+without_keyword = data[~contains_keyword]
+
+print(f"\ncontaining {keyword}: {len(with_keyword)}")
+print(with_keyword['job_title'].head().to_list())
+
+print(f"\nnot containing {keyword}: {len(without_keyword)}")
+print(without_keyword['job_title'].head().to_list())
+
+#seems like there are more non-engineers than engineers in general.
